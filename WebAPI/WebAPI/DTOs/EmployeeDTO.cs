8fe4f2f9539ddab2fd0dbcdb50a2816104f0a7f6
Habiba El-Sayed <habiba192213@fci.bu.edu.eg>
2024-05-17 00:22:26 +0300

    fixing issue #3

--- WebAPI/WebAPI/DTOs/EmployeeDTO.cs
+++ WebAPI/WebAPI/DTOs/EmployeeDTO.cs
@@ -1,28 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using WebAPI.Constants;
 using WebAPI.Models;
 
 namespace WebAPI.DTOs
 {
     public class EmployeeDTO
     {
-        public Guid ID { get; set; }
-        public double SSN { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
-        public int Gender { get; set; }
-        public string Headline { get; set; }
-        public string Country { get; set; }
-        public DateOnly BirthDate { get; set; }
-        public DateOnly JoinDate { get; set; }
-        public decimal FixedSalary { get; set; }
-        public decimal GrossSalary { get; set; }
-        public TimeOnly Arrival { get; set; }
-        public TimeOnly Departure { get; set; }
-        public DateOnly? LeftDate { get; set; }
-        public string ShiftName { get; set; }
-        public string departmentName { get; set; }
+        public string id { get; set; }
+        public double ssn { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string phoneNumber { get; set; }
+        public Gender gender { get; set; }
+        public string headline { get; set; }
+        public string country { get; set; }
+        public DateOnly birthDate { get; set; }
+        public DateOnly joinDate { get; set; }
+        public DateOnly? leftDate { get; set; }
+        public decimal fixedSalary { get; set; }
+        public decimal grossSalary { get; set; }
+        public decimal baseSalary { get; set; }
+        public ShiftDTO shiftDTO { get; set; }
+        public DepartmentDTO departmentDTO { get; set; }
+        public DateOnly createdAt { get; set; }
+        public DateOnly? lastModified { get; set; }
+        public UserDTO createdBy { get; set; }
     }
 }
--- WebAPI/WebAPI/Models/Employee.cs
+++ WebAPI/WebAPI/Models/Employee.cs
@@ -7,41 +7,36 @@ namespace WebAPI.Models
     public class Employee
     {
         [Key]
-        public int Id { get; set; }
-
-        public Guid ExposedID { get; set; }
-
-        public double SSN { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Headline { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
-        public Gender Gender { get; set; }
-        public string Country { get; set; }
-        public DateOnly BirthDate { get; set; }
-        public DateOnly JoinDate { get; set; }
-
+        public int id { get; set; }
+        public string exposedID { get; set; }
+        public double ssn { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string headline { get; set; }
+        public string address { get; set; }
+        public string phoneNumber { get; set; }
+        [Column(TypeName = "nvarchar(24)")]
+        public Gender gender { get; set; }
+        public string country { get; set; }
+        public DateOnly birthDate { get; set; }
+        public DateOnly joinDate { get; set; }
+        public DateOnly? leftDate { get; set; }
         [Column(TypeName = "money")]
-        public decimal FixedSalary { get; set; }
-
+        public decimal fixedSalary { get; set; }
         [Column(TypeName = "money")]
-        public decimal GrossSalary { get; set; }
-        public TimeOnly Arrival { get; set; }
-        public TimeOnly Departure { get; set; }
-
-        [ForeignKey("Department")]
-        public int? DeptId { get; set; }
-
-        // Navigation Properties
-        public  virtual Department Department { get; set; }
-
-        [ForeignKey("Shift")]
-        public string ShiftId { get; set; }
-        public virtual Shift Shift { get; set; }
-
-        public DateOnly? LeftDate { get; set; }
-
-
+        public decimal grossSalary { get; set; }
+        [Column(TypeName = "money")]
+        public decimal baseSalary { get; set; }
+        [ForeignKey("department")]
+        public int? deptId { get; set; }
+        public  virtual Department department { get; set; }
+        [ForeignKey("shift")]
+        public string shiftId { get; set; }
+        public virtual Shift shift { get; set; }
+        public DateOnly createdAt { get; set; }
+        public DateOnly? lastModified { get; set; }
+        [ForeignKey("createdBy")]
+        public string createdById { get; set; }
+        public virtual ApplicationUser createdBy { get; set; }
     }
 }
